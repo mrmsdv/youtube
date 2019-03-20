2e0b2e2e7ef3416a9ba2545b26a99682b885084d
Mr_MSDV <37757663+mrmsdv@users.noreply.github.com>
2019-03-20 22:57:39 +0800

    Add files via upload

---
+++ mp3.py
@@ -0,0 +1,24 @@
+# -- encoding: utf-8 --
+import os, sys
+import time , random
+
+
+def ketik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.5)
+
+print "sebelumnya sudah pernah install Tools Ini ? Y/n ?"
+n = raw_input("masukan pilihan : ")
+if n == 'n':
+	print "install Program "
+	os.system("sh install.sh")
+if n == 'y':
+	ketik("waiting.....")
+	ketik("mau loading bentar . . . ")
+	os.system("clear")
+	print "kalau ingin search ketikan comand"
+	print "search nama_lagu.mp3"
+	os.system("mpsyt")
+
